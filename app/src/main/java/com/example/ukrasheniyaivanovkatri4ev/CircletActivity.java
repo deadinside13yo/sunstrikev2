04fdb386293dce580f0c09fb09389bb86310caa9
student <Andrei>
2021-01-22 12:21:44 +0500

    Initial commit

---
+++ app/src/main/java/com/example/ukrasheniyaivanovkatri4ev/CircletActivity.java
@@ -0,0 +1,14 @@
+package com.example.ukrasheniyaivanovkatri4ev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CircletActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circlet);
+    }
+}
---
+++ app/src/main/java/com/example/ukrasheniyaivanovkatri4ev/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ukrasheniyaivanovkatri4ev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void BracerClick(View view) {
+        Intent intent=new Intent(MainActivity.this, BracerActivity.class);
+        startActivity(intent);
+    }
+    public void RingClick(View view) {
+        Intent intent=new Intent(MainActivity.this,RingActivity.class);
+        startActivity(intent);
+    }
+    public void CircletClick(View view) {
+        Intent intent=new Intent(MainActivity.this,CircletActivity.class);
+        startActivity(intent);
+    }
+}
