2f4cc065fcae54da95cc5fe460487046ccf4e72a
student <@mail.ru>
2021-01-23 15:00:59 +0500

    po4tiend

--- app/src/main/java/com/example/ukrasheniyaivanovkatri4ev/BracerActivity.java
+++ app/src/main/java/com/example/ukrasheniyaivanovkatri4ev/BracerActivity.java
@@ -1,14 +1,12 @@
 package com.example.ukrasheniyaivanovkatri4ev;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
